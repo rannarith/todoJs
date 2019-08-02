bfaec9735848bf206b91fdfac822f9c0830c30f4
qoodot <dev.qoodot@gmail.com>
2019-08-02 19:16:12 +0700

    add selector

--- js/main.js
+++ js/main.js
@@ -9,3 +9,13 @@
 
 const todolists = document.querySelectorAll('.todolist');
 todolists.forEach((todolist) => console.log(todolist));
+
+const ul = document.querySelector('.todolists');
+// ul.remove();
+//ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Never give up';
+ul.lastElementChild.innerHTML = '<h2>Stay Focus</h2>';
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'gray';
